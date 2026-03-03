e4cf8f994a209c542610a4816ef99aa54aa9c419
farhinkhanam <farhinkhanam12@gmail.com>
2026-03-03 15:27:23 +0530

    Fixed gunicorn port issue

--- app.py
+++ app.py
@@ -248,8 +248,3 @@ def edit_booking(id):
 
 
 # ---------------- RUN ----------------
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
